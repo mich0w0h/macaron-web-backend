317d214bce11f3e555a39250f95cb821c855745e
mich0w0h <aokimiho71@gmail.com>
2024-02-25 06:50:38 +0900

    initial commit

---
+++ modules/langchainHandler.ts
@@ -0,0 +1,117 @@
+import { ChatOpenAI } from "npm:@langchain/openai";
+import { RunInput } from "npm:@langchain/core";
+import {
+  FewShotPromptTemplate,
+  PromptTemplate,
+} from "npm:@langchain/core/prompts";
+import { StringOutputParser } from "npm:@langchain/core/output_parsers";
+import { csvToFlatArray } from "./csvParser.ts";
+import { containsKanji } from "./characterChecker.ts";
+
+const model = new ChatOpenAI({
+  modelName: "gpt-3.5-turbo",
+  maxTokens: 50,
+});
+
+const outputParser = new StringOutputParser();
+
+const characterLines = await csvToFlatArray("./data/line_examples.csv");
+
+function createPromptFromLines(
+  characterLines: string[],
+): FewShotPromptTemplate {
+  const examples = characterLines.map((line) => {
+    return { "answer": line }; // answer is the label for the example
+  });
+
+  const examplePrompt = new PromptTemplate({
+    template: "{answer}",
+    inputVariables: ["answer"],
+  });
+
+  const prefix =
+    `ユーザーの入力に対し、マカロンというキャラクターの返答を生成してください。
+マカロンは世間知らずなキャラクターで、一人称は「マカロン」です。
+返答は10文字以内で作成してください。話し方の例はこんな感じです：`;
+  const suffix = `
+ユーザー： {commentText}
+マカロン：
+`;
+
+  const fewShotPrompt = new FewShotPromptTemplate({
+    examples,
+    examplePrompt,
+    prefix: prefix,
+    suffix: suffix,
+    inputVariables: ["commentText"],
+  });
+
+  return fewShotPrompt;
+}
+
+function createPromptToConvertKanji(): FewShotPromptTemplate {
+  const examples = [
+    { "input": "私は猫が好きです。", "output": "わたしはねこがすきです。" },
+    {
+      "input": "私はお金が欲しいです。",
+      "output": "わたしはおかねがほしいです。",
+    },
+    // カタカナはそのままにする
+    {
+      "input": "私はスイカが好きです。",
+      "output": "わたしはスイカがすきです。",
+    },
+    { "input": "名前はマカロンだよ！", "output": "なまえはマカロンだよ！" },
+    {
+      "input": "マカロンの誕生日はいつだ！",
+      "output": "マカロンのたんじょうびはいつだ！",
+    },
+  ];
+
+  const examplePrompt = new PromptTemplate({
+    template: "入力：{input}\n出力：{output}",
+    inputVariables: ["input", "output"],
+  });
+
+  const fewShotPrompt = new FewShotPromptTemplate({
+    examples,
+    examplePrompt,
+    prefix:
+      "次の文章の漢字をひらがなに変換してください。カタカナは変換しないでカタカナのまま出力してください。",
+    suffix: "入力：{input_string}\n出力：",
+    inputVariables: ["input_string"],
+  });
+
+  return fewShotPrompt;
+}
+
+async function invokeFewShot(
+  promptTemplate: FewShotPromptTemplate,
+  chainInput: RunInput,
+): Promise<string> {
+  const chain = promptTemplate.pipe(model).pipe(outputParser);
+  const stringResult = await chain.invoke(chainInput) as string;
+
+  console.log("[LangChain] response generated: ", stringResult);
+
+  return stringResult;
+}
+
+export async function generateLLMResponse(
+  commentText: string,
+): Promise<string> {
+  const promptTemplate = createPromptFromLines(characterLines);
+
+  let stringResult: string = await invokeFewShot(promptTemplate, {
+    commentText: commentText,
+  });
+
+  if (containsKanji(stringResult)) {
+    const kanjiConversionPrompt = createPromptToConvertKanji();
+    stringResult = await invokeFewShot(kanjiConversionPrompt, {
+      input_string: stringResult,
+    });
+  }
+
+  return stringResult;
+}
---
+++ server.ts
@@ -0,0 +1,54 @@
+// Code to create a server using Oak framework with deno and handle requests from the frontend
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { RateLimiter } from "https://deno.land/x/oak_rate_limit/mod.ts";
+import { generateLLMResponse } from "./modules/langchainHandler.ts";
+import { CharacterResponse, UserComment } from "./types/index.d.ts";
+
+const app = new Application();
+
+// Set up rate limiting middleware
+const rateLimit = RateLimiter({
+  windowMs: 1000,
+  max: 100,
+});
+
+// make sure to initialize middlewares before the router
+app.use(await rateLimit);
+
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Hello world!";
+  })
+  // receive a user comment and return a response
+  .post("/generate", async (context) => {
+    try {
+      const body = await context.request.body.text();
+      const userComment: UserComment = JSON.parse(body);
+
+      const responseText: string = await generateLLMResponse(userComment.text);
+
+      const characterResponse: CharacterResponse = {
+        text: responseText,
+      };
+
+      context.response.body = JSON.stringify(characterResponse);
+      context.response.type = "json";
+      context.response.status = 200;
+
+      console.log("[deno router] response sent to frontend");
+    } catch (error) {
+      console.error("[deno router] error occurred:", error);
+
+      context.response.body = {
+        error: "An error occurred while processing the request.",
+      };
+      context.response.status = 500;
+    }
+  });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log(`Server running on http://localhost:8000`);
+await app.listen({ port: 8000 });
